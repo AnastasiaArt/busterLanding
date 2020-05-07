a7f59a613163596103e9f108a47dc6316152304f
Artemyeva Anastasiya <a.artemyeva@leadgid.ru>
2020-05-07 19:16:24 +0300

    created page, add style

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                custom: {
+                    'blue': '#538797',
+                    'dark-blue': '#122B3F',
+                    'light-blue': '#C2D9CD',
+                    'yellow': '#FED55D',
+                    'orange': '#FF842A',
+                    'white': '#FCFAF1',
+                }
+            }
+        },
+        container: {
+            center: true,
+            padding: {
+                default: '1rem',
+                sm: '2rem',
+                lg: '8%',
+                xl: '9.4%',
+            },
+        },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1366px',
+        },
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+// Webpack v4
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use:  [  'style-loader', MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false
+                        }
+                    },'postcss-loader']
+            },
+            {
+                test: /images[\\\/].+\.(gif|png|jpe?g|svg)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'images/[name][hash].[ext]'
+                    }
+                }, {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        mozjpeg: {
+                            progressive: true,
+                            quality: 70
+                        }
+                    }
+                },
+                ],
+            }, {
+                test: /fonts[\\\/].+\.(otf|eot|svg|ttf|woff|woff2)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'fonts/[name].[ext]'
+                    }
+                },
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+
+        new WebpackMd5Hash(),
+        new CopyWebpackPlugin([
+            {
+                from: './src/images',
+                to: './images'
+            },
+            {
+                from: './src/fonts',
+                to: './fonts'
+            }
+        ])
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 9000,
+        compress: true,
+        open: true
+    }
+};
